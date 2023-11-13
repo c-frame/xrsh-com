1539df1fefc87e1bbe34b729cc05ef8e26909c8a
Leon van Kammen <leonvankammen@gmail.com>
2023-11-13 10:09:38 +0100

    work in progress [might break]

--- com/require.js
+++ com/require.js
@@ -1,8 +1,12 @@
-AFRAME.registerComponent('xrshell', {
-  init: function ( ) { 
-
-    console.log("this is the boot component which initializes other components")
-
+AFRAME.registerComponent('require', {
+  init: function() { 
   },
 
-});
+})
+    //<script src="https://coderofsalvation.github.io/xrsh-apps/helloworld.js"></script> 
+const updateComponents = AFRAME.AEntity.prototype.updateComponents
+AFRAME.AEntity.prototype.updateComponents = function(updateComponents){
+  return function(){
+    return updateComponents.apply(this,args)
+  }
+}(updateComponents)
