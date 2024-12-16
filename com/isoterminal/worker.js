88e3d4f04d7287184c3ce1acd107a9add127eb98
Leon van Kammen <leonvankammen@gmail.com>
2024-12-16 17:16:01 +0000

    workaround for file-operations with /mnt-prefix

--- com/isoterminal/worker.js
+++ com/isoterminal/worker.js
@@ -42,11 +42,17 @@ this.runISO = function(opts){
   /* 
    * forward events/functions so non-worker world can reach them
    */
-  this.create_file     = async function(){ return emulator.create_file.apply(emulator, arguments[0]) }
-  this.read_file       = async function(){ return emulator.read_file.apply(emulator, arguments[0])   }
-  this.read_file_world = async function(){ return emulator.fs9p.read_file_world.apply(emulator.fs9p, arguments[0])   }
-  this.append_file     = async function(){ emulator.fs9p.append_file.apply(emulator.fs9p, arguments[0]) }
-  this.update_file     = async function(){ emulator.fs9p.update_file.apply(emulator.fs9p, arguments[0]) }
+ 
+  // stripping '/mnt' is needed (the 9p mounted fs does not know about this)
+  const stripMountDir  = (arr) => {
+    arr[0] = String(arr[0]).replace(/^\/mnt/,'')
+    return arr
+  }
+  this.create_file     = async function(){ return emulator.create_file.apply(emulator, stripMountDir(arguments[0]) ) }
+  this.read_file       = async function(){ return emulator.read_file.apply(emulator, stripMountDir(arguments[0]) )   }
+  this.read_file_world = async function(){ return emulator.fs9p.read_file_world.apply(emulator.fs9p, stripMountDir(arguments[0]) )   }
+  this.append_file     = async function(){ emulator.fs9p.append_file.apply(emulator.fs9p, stripMountDir(arguments[0])) }
+  this.update_file     = async function(){ emulator.fs9p.update_file.apply(emulator.fs9p, stripMountDir(arguments[0])) }
 
   // filename will be read from 9pfs: "/mnt/"+filename
   emulator.readFromPipe = function(filename,cb){
